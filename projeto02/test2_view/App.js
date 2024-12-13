4ded87de5203697c871c321e3b08b787efc8a3c8
Ermersongomes <ermersonwwe@gmail.com>
2024-12-13 10:03:13 -0300

    new

---
+++ projeto02/test2_view/App.js
@@ -0,0 +1,10 @@
+import{View, Text} from "react-native";
+export default function App() {
+  return(
+    <View style={{display: "flex", justifyContent:"center", alignContent: "center"}}>
+        <Text style={{backgroundColor: "red", height:150, width:150}}>
+            olá mundo
+        </Text>
+    </View>
+  )
+}
